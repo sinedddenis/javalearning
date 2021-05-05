ab06d531df7ac38b49129294089988e8f2a40bef
Denys <stasiydnepro@gmail.com>
2021-05-05 15:01:57 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt('How many film have you watched?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    private: false
+};
+ const a = prompt('Last movie?',''),
+ b = prompt('Rating',''),
+ c = prompt('Last movie?',''),
+ d = prompt('Rating','');
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+
+ console.log(personalMovieDB);
